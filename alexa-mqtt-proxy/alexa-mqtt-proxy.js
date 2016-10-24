f25f72e255c753cf01c220ee1bb07b17373f3517
Jag Bains <git@jagbains.org>
2016-10-24 20:42:54 +0100

    Amended MIT license section

--- alexa-mqtt-proxy/alexa-mqtt-proxy.js
+++ alexa-mqtt-proxy/alexa-mqtt-proxy.js
@@ -2,7 +2,7 @@
 
 /*
 The MIT License (MIT)
-Copyright (c) 2015 Maker Musings && m0ngr31
+Copyright (c) 2016 jagsta
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
